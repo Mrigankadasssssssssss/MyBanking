33e65b3ba97bc3018bab5cc35d31986733e23fd3
Mriganka Das <dasmriganka.2002@gmail.com>
2024-05-12 12:28:28 +0530

    feature: SideBars and Routing are implemented
    author: Mriganka Das
    
    <dasmriganka.2002@gmail.com>

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import HeaderBox from '../../components/HeaderBox';
-import TotalBalanceBox from '@/components/TotalBalanceBox';
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from "react";
+import HeaderBox from "../../components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 
 const Home = () => {
-  const loggedIn = {firstName:'Mriganka'}  ;
+  const loggedIn = {
+    firstName: "Mriganka",
+    lastName: "Das",
+    email: "dasmriganka.2002@gmail.com",
+  };
 
   return (
-    <section className='home'>
-        <div className='home-content'>
-            <header className='home-header'>
-                <HeaderBox
-                    type="greeting"
-                    title="Welcome"
-                    user={loggedIn?.firstName||'Guest'}
-                    subtext="Access and manage your account and transaction efficiently."
-                />
-                <TotalBalanceBox
-                    accounts = {[]}
-                    totalBanks = {1}
-                    totalCurrentBalance = {2000}
-                />
-            </header>
-        </div>
-    </section>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transaction efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={2000}
+          />
+        </header>
 
-export default Home
+        {/* Recent Transaction */}
+      </div>
+      {/* right sidebar */}
+      
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 600.5 }]}
+      />
+    </section>
+  );
+};
+
+export default Home;
